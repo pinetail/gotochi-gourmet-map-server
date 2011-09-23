89920060290a26a4f67cae2f6cb8db404523b508
pinetail <civic.typexs@gmail.com>
2011-09-23 23:54:59 +0900

    modify timezone

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module GotochiGourmetMapServer
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = 'Tokyo'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -38,5 +40,6 @@ module GotochiGourmetMapServer
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
   end
 end
